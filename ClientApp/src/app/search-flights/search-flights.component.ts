899338c030e8e282c0e12b93fc0139ca0595e33d
cherryis <rsoonae.yahoo.com>
2023-09-07 21:55:44 -0700

    Add project files.

---
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -0,0 +1,23 @@
+import { Time } from '@angular/common';
+import { Component } from '@angular/core';
+import { FlightService } from './../api/services/flight.service';
+import { FlightRm } from '../api/models';
+
+@Component({
+  selector: 'app-search-flights',
+  templateUrl: './search-flights.component.html',
+  styleUrls: ['./search-flights.component.css']
+})
+export class SearchFlightsComponent {
+  searchResult: FlightRm[] = []
+  constructor(private flightService: FlightService) { }
+
+  search() {
+    this.flightService.searchFlight({}).subscribe(response => this.searchResult = response, this.handleError)
+  }
+
+  private handleError(err: any) {
+    console.log(err)
+  }
+}
+
