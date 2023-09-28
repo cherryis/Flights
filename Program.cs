eea79a5e4020efec0074ae65a5a3f65dbcc6acea
cherryis <rsoonae.yahoo.com>
2023-09-28 14:41:33 -0700

    Added new folder Data
    Created Entities class and object
     Modified files=>
    Program.cs, FlightController.cs, PassengerController.cs, Entities.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.OpenApi.Models;
+using Flights.Data;
+using Flights.Domain.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,7 +17,66 @@ builder.Services.AddSwaggerGen( c =>
     c.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["action"] + e.ActionDescriptor.RouteValues["controller"]}");
 });
 
+builder.Services.AddSingleton<Entities>();
+
 var app = builder.Build();
+
+var entities = app.Services.CreateScope().ServiceProvider.GetService<Entities>();
+var random = new Random();
+
+Flight[] flightsToSeed = new Flight[]
+{
+           new (   Guid.NewGuid(),
+                "American Airlines",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Los Angeles, CA",DateTime.Now.AddHours(random.Next(1, 3))),
+                new TimePlace("Seattle, WA",DateTime.Now.AddHours(random.Next(4, 10))),
+                    2),
+        new (   Guid.NewGuid(),
+                "Alaska Airline",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Seattle, WA",DateTime.Now.AddHours(random.Next(1, 10))),
+                new TimePlace("Anchorage, AK",DateTime.Now.AddHours(random.Next(4, 15))),
+                random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+    "Delta",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Houston, TX",DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlace("Seattle, WA",DateTime.Now.AddHours(random.Next(4, 18))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "Sprit Airline",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Orlando, FL",DateTime.Now.AddHours(random.Next(1, 21))),
+                new TimePlace("New Orleans, LA",DateTime.Now.AddHours(random.Next(4, 21))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "Korean Airline",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Inchon, KR",DateTime.Now.AddHours(random.Next(1, 23))),
+                new TimePlace("Los Angeles, CA",DateTime.Now.AddHours(random.Next(4, 25))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "United",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Minneapolis, MN",DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlace("New Orleans, LA",DateTime.Now.AddHours(random.Next(4, 19))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "ABA Air",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Praha Ruzyne",DateTime.Now.AddHours(random.Next(1, 55))),
+                new TimePlace("Paris",DateTime.Now.AddHours(random.Next(4, 58))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "AB Corporate Aviation",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Le Bourget",DateTime.Now.AddHours(random.Next(1, 58))),
+                new TimePlace("Zagreb",DateTime.Now.AddHours(random.Next(4, 60))),
+                    random.Next(1, 853))
+
+};
+entities.Flights.AddRange(flightsToSeed);
 
 app.UseCors(builder => builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader());
 
